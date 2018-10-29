b499071dd8274e608fbaf15361d8b5a36f765a65
Dominik Eckert <dominik.17.eckert@fau.de>
2018-10-29 16:08:23 +0100

    circle als klasse

--- circle.py
+++ circle.py
@@ -1,24 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-resolution = 100
-radius = 10
-z_x = 30
-z_y = 30
+class Circle:
+    def __init__(self, resolution, radius, position):
+        self.resolution = resolution
+        self.radius = radius
+        self.position = position
 
-'Create a Black Image________________________________'
-v_black = np.zeros([1,resolution**2]).transpose()
-v_rgb = np.hstack([v_black, v_black, v_black])
-m_rgb = np.reshape(v_rgb, [resolution, resolution,3])
+    def draw(self):
 
-'Create Circle_______________________________________'
-for y in np.arange(resolution):
-    a_min = np.round(-np.sqrt(radius**2 - (y - z_y)**2) + z_x)
-    a_max = np.round(np.sqrt(radius ** 2 - (y - z_y) ** 2) + z_x)
-    print('min: ', a_min, 'max: ', a_max)
+        # Create a Black Image________________________________
+        v_black = np.zeros([1, self.resolution**2]).transpose()
+        v_rgb = np.hstack([v_black, v_black, v_black])
+        self.m_rgb = np.reshape(v_rgb, [self.resolution, self.resolution, 3])
 
-    if not (np.isnan(a_min) or np.isnan(a_max) or a_min == a_max):
-        m_rgb[y][int(a_min):int(a_max)] = [1.0, 1.0, 1.0]
+        #Create Circle_______________________________________'
+        for y in np.arange(self.resolution):
+            x_min = np.round(-np.sqrt(self.radius**2 - (y - self.position[1])**2) + self.position[0])
+            x_max = np.round(np.sqrt(self.radius ** 2 - (y - self.position[1]) ** 2) + self.position[0])
 
-'Plot Picture'
-plt.imshow(m_rgb)
+            if not (np.isnan(x_min) or np.isnan(x_max) or x_min == x_max):
+                self.m_rgb[y][int(x_min):int(x_max)] = [1.0, 1.0, 1.0]
+
+    def show(self):
+        plt.imshow(self.m_rgb)
+
+
+if __name__ == '__main__':
+    circle = Circle(1000, 300, [400,600])
+    circle.draw()
+    circle.show()
+
