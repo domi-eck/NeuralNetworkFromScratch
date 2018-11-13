ed48f57ba22f87eda6f6160c5302c4f1b468ac2f
schwarza <alexander.sch@gmx.de>
2018-11-13 14:25:59 +0100

    softMax forward

--- Exercise1/src/Layers/SoftMax.py
+++ Exercise1/src/Layers/SoftMax.py
@@ -2,7 +2,22 @@ import numpy as np
 
 class SoftMax:
     def forward(self, input_tensor, label_tensor):
-        a = 2
+        #input x
+        #label y
+        #predlabel y
+        self.input = input_tensor
+        a = np.exp(input_tensor)
+
+        colum_sums = 1/ np.sum(a, 0)
+        colum_sums = np.diag(colum_sums)
+
+        y_h = np.dot(a, colum_sums)
+        loss = 0
+        lossA = label_tensor
+        lossA = np.where(lossA == 1, -1 * np.log( y_h ) ,0)
+
+
+        return np.sum(lossA)
     def backward(self, label_tensor):
         a =2
     def predict(self, input_tensor):
