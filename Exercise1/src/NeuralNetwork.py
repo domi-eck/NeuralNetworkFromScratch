3f06af2c19656c017a1fba1c8cdad92f5ec7bd29
schwarza <alexander.sch@gmx.de>
2018-11-18 17:51:08 +0100

    anlegen der NeuralNetwork Klasse

---
+++ Exercise1/src/NeuralNetwork.py
@@ -0,0 +1,17 @@
+import numpy as np
+import  Exercise1.src.Layers.FullyConnected as fc
+
+class NeuralNetwork:
+    def __init__(self):
+        self.loss = list()
+        self.layers = list()
+        self.data_layer = []
+        self.loss_layer = []
+    def forward(self):
+        a = list()
+    def backward(self):
+        a = 3
+    def train(self, iterations):
+        a = 0
+    def test(self, input_tensor):
+        a = 0
