1a4757b12a2218c41b8e76da2df82d11a1e50359
Dominik Eckert <dominik.17.eckert@fau.de>
2018-11-29 14:45:12 +0100

    convolution with one stride and one kernel is working

--- Layers/Conv.py
+++ Layers/Conv.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import signal as sig
 
 class Conv:
     def __init__(self, stride_shape, convulution_shape, num_kernels, learning_rate):
@@ -7,60 +8,90 @@ class Conv:
         self.num_kernels = num_kernels
         self.learning_rate = learning_rate
         self.weights = np.array([])
-        self.weights = np.ones(convulution_shape)
+        self.weights = np.ones([num_kernels, *convulution_shape])
 
 
     def forward(self, input_tensor):
         #loop for every batch
-        for b in np.arange(input_tensor.shape[0]):
-
-            #determine end of indices for convultion
-            input_x_size = input_tensor.shape[3]   #starting with 0
-            input_y_size = input_tensor.shape[2]   #starting with 0
-            x_stride = self.stride_shape[1]
-            y_stride = self.stride_shape[0]
-            conv_x_size = self.conv_shape[2]
-            conv_y_size = self.conv_shape[1]
-            # take input tensor for one batch
-            one_batch_input_tensor = input_tensor[b]
+        # determine end of indices for convultion
+        input_x_size = input_tensor.shape[3]  # starting with 0
+        input_y_size = input_tensor.shape[2]  # starting with 0
+        x_stride = self.stride_shape[1]
+        y_stride = self.stride_shape[0]
+        conv_x_size = self.conv_shape[2]
+        conv_y_size = self.conv_shape[1]
+        # take input tensor for one batch
 
 
+        for batch in np.arange(input_tensor.shape[0]):
 
-            #calculate x boarder with stride size of one
-            end_x_boarder = input_x_size - conv_x_size
-            last_x_index = input_x_size - conv_x_size
-            #calculate how the last stride has to end so that no x values are missing in the convolution
-            if not end_x_boarder % x_stride is 0:
-                last_x_stride = end_x_boarder + (x_stride - end_x_boarder % x_stride)
-                # calculate the zeros which has to be padded that the last stride is possible
-                zero_x_padding = last_x_stride + conv_x_size - input_x_size
-                padding_array = np.zeros([one_batch_input_tensor.shape[0], one_batch_input_tensor.shape[1], 2 ])
-                one_batch_input_tensor = np.concatenate([one_batch_input_tensor, padding_array], 2)
+            # #calculate x boarder with stride size of one
+            # end_x_boarder = input_x_size - conv_x_size
+            # last_x_index = input_x_size - conv_x_size
+            # #calculate how the last stride has to end so that no x values are missing in the convolution
+            # if not end_x_boarder % x_stride is 0:
+            #     last_x_stride = end_x_boarder + (x_stride - end_x_boarder % x_stride)
+            #     # calculate the zeros which has to be padded that the last stride is possible
+            #     zero_x_padding = last_x_stride + conv_x_size - input_x_size
+            #     padding_array = np.zeros([one_batch_input_tensor.shape[0], one_batch_input_tensor.shape[1], 2 ])
+            #     one_batch_input_tensor = np.concatenate([one_batch_input_tensor, padding_array], 2)
+            #
+            # else:
+            #     last_x_stride = end_x_boarder
+            #
+            #
+            #
+            # #same as for x
+            # end_y_boarder = input_y_size - conv_y_size
+            # if not end_y_boarder % y_stride is 0:
+            #     last_y_stride = end_y_boarder + (y_stride - end_y_boarder % y_stride)
+            #     zero_y_padding = last_y_stride + conv_y_size - input_y_size
+            #     padding_array = np.zeros( [one_batch_input_tensor.shape[0], 1, one_batch_input_tensor.shape[2]])
+            #     one_batch_input_tensor = np.concatenate([one_batch_input_tensor, padding_array], 1)
+            # else:
+            #     last_y_stride = end_y_boarder
+            #
+            # end_y = last_y_stride / y_stride +1 #+1 cause of the zero stride
+            # end_x = last_x_stride / x_stride +1
+            #
+            # output_tensor = np.zeros([int(end_y), int(end_x)])
+            #
+            # #ToDo only one Kernel is used
+            # #ToDo for some reason channel size is not reduced
+            # #ToDo do calculation new
+            #
+            out_x = np.int(np.ceil(input_x_size/x_stride))
+            out_y = np.int(np.ceil(input_y_size/y_stride))
+            #
+            output_tensor = np.zeros([out_y, out_x])
+            #
+            # padding_y_array = np.zeros([one_batch_input_tensor.shape[0], conv_y_size, one_batch_input_tensor.shape[2]])
+            # padding_x_array = np.zeros([one_batch_input_tensor.shape[0], one_batch_input_tensor.shape[1], conv_x_size])
+            #
+            # one_batch_input_tensor = np.concatenate([one_batch_input_tensor, padding_y_array], 1)
+            # one_batch_input_tensor = np.concatenate([one_batch_input_tensor, padding_x_array], 2)
 
-            else:
-                last_x_stride = end_x_boarder
+            '''Same Padding:  tries to pad evenly left and right, but if the amount of columns to be added is odd, 
+            it will add the extra column to the right, as is the case in this example (the same logic applies 
+            vertically: there may be an extra row of zeros at the bottom). In Same padding with stride length one 
+            the dimensions stay the same'''
 
+            #Try new Approach, at first only with stride 1
+            x_left_padding = np.zeros([input_tensor[batch].shape[0], input_tensor[batch].shape[1],
+                                       np.int(np.floor((conv_x_size-1)/2))])
+            x_right_padding = np.zeros([input_tensor[batch].shape[0], input_tensor[batch].shape[1],
+                                       np.int(np.ceil((conv_x_size-1)/2))])
 
+            padded_input_tensor = np.concatenate([x_left_padding, input_tensor[batch]], 2)
+            padded_input_tensor = np.concatenate([padded_input_tensor, x_right_padding], 2)
 
-            #same as for x
-            end_y_boarder = input_y_size - conv_y_size
-            if not end_y_boarder % y_stride is 0:
-                last_y_stride = end_y_boarder + (y_stride - end_y_boarder % y_stride)
-                zero_y_padding = last_y_stride + conv_y_size - input_y_size
-                padding_array = np.zeros( [one_batch_input_tensor.shape[0], 1, one_batch_input_tensor.shape[2]])
-                one_batch_input_tensor = np.concatenate([one_batch_input_tensor, padding_array], 1)
-            else:
-                last_y_stride = end_y_boarder
+            y_left_padding = np.zeros([padded_input_tensor.shape[0], np.int(np.floor((conv_y_size-1)/2)),
+                                       padded_input_tensor.shape[2]])
+            y_right_padding = np.zeros([padded_input_tensor.shape[0], np.int(np.ceil((conv_y_size-1)/2)),
+                                        padded_input_tensor.shape[2]])
 
-            end_y = last_y_stride / y_stride +1 #+1 cause of the zero stride
-            end_x = last_x_stride / x_stride +1
-
-            output_tensor = np.zeros([int(end_y), int(end_x)])
-
-            ToDo only one Kernel is used
-            ToDo for some reason channel size is not reduced
-
-
+            padded_input_tensor = np.concatenate([y_left_padding, padded_input_tensor], 1)
+            padded_input_tensor = np.concatenate([padded_input_tensor, y_right_padding], 1)
 
 
 
@@ -75,21 +106,26 @@ class Conv:
 
             #loop over every y value with the right stride size
             y_out = 0
-            for y in np.arange(0, last_y_stride + y_stride , y_stride):
-                #loop over every x value with the right stride size
-                x_out = 0
-                for x in np.arange(0, last_x_stride + x_stride, x_stride):
+            for y in np.arange(0, input_y_size, y_stride):
+                 #loop over every x value with the right stride size
+                 x_out = 0
+                 for x in np.arange(0, input_x_size, x_stride):
+            #
+            #         #get the tensor which will be multiplied with the kernel
+                     tensor_for_multiply = padded_input_tensor[:, y: (y + conv_y_size), x:(x + conv_x_size)]
+            #         #make one convulution, first multiply with the weights and the sum over it to get one value
+                     output_tensor[y_out][x_out] = np.sum(np.multiply(tensor_for_multiply, self.weights))
+            #
+                     x_out += 1
 
-                    #get the tensor which will be multiplied with the kernel
-                    tensor_for_multiply = one_batch_input_tensor[:, y: (y + conv_y_size), x:(x + conv_x_size)]
-                    #make one convulution, first multiply with the weights and the sum over it to get one value
-                    output_tensor[y_out][x_out] = np.sum(np.multiply(tensor_for_multiply, self.weights))
+                 y_out += 1
 
-                    x_out += 1
+            #works only with stride 1
+            #TODO make with four kernels
+            #TODO make for each batch
+            output_tensor = sig.convolve(input_tensor[b], self.weights, mode = "same")
 
-                y_out += 1
-
-        return input_tensor
+        return output_tensor
 
 
     def backward(self, backward_tensor):
