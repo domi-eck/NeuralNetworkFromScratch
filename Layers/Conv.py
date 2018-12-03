709ead99bb3182daf3eefa709b423a05e21bc4f8
Dominik Eckert <dominik.17.eckert@fau.de>
2018-12-03 17:23:30 +0100

    changed optimizer

--- Layers/Conv.py
+++ Layers/Conv.py
@@ -1,4 +1,5 @@
 import numpy as np
+from Optimization import Optimizers
 
 
 class Conv:
@@ -27,6 +28,16 @@ class Conv:
         self.out_x = 0
         self.out_y = 0
 
+
+    def set_optimizer(self, optimizer):
+        self.optimizer = optimizer
+
+    def initialize(self, weights_initializer, bias_initializer):
+        fan_in = np.product(self.weights[0].shape)
+        fan_out = np.product([*self.weights[0][0].shape, self.num_kernels])
+        self.weights = weights_initializer.initialize(self.weights.shape, fan_in, fan_out)
+
+
     def forward(self, input_tensor):
 
         # if 1D array, add one dimension
@@ -45,6 +56,37 @@ class Conv:
         self.conv_y_size = self.conv_shape[1]
         self.batch_num = input_tensor.shape[0]
 
+        '''Same Padding: tries to pad evenly left and right, but if the amount of columns to be added is odd, 
+        it will add the extra column to the right, as is the case in this example (the same logic applies 
+        vertically: there may be an extra row of zeros at the bottom). In Same padding with stride length one 
+        the dimensions stay the same'''
+
+        '''Same - Padding is calculated as described above'''
+        self.num_x_left_zeros = np.int(np.floor((self.conv_x_size - 1) / 2))
+        self.num_x_right_zeros = np.int(np.ceil((self.conv_x_size - 1) / 2))
+
+        self.num_y_left_zeros = np.int(np.floor((self.conv_y_size - 1) / 2))
+        self.num_y_right_zeros = np.int(np.ceil((self.conv_y_size - 1) / 2))
+
+
+        x_left_padding = np.zeros([input_tensor.shape[0], input_tensor.shape[1], input_tensor.shape[2],
+                                   np.int(np.floor((self.conv_x_size - 1) / 2))])
+        x_right_padding = np.zeros([input_tensor.shape[0], input_tensor.shape[1], input_tensor.shape[2],
+                                    np.int(np.ceil((self.conv_x_size - 1) / 2))])
+
+        self.padded_input_tensor = np.concatenate([x_left_padding, input_tensor], 3)
+        self.padded_input_tensor = np.concatenate([self.padded_input_tensor, x_right_padding], 3)
+
+        y_left_padding = np.zeros([self.padded_input_tensor.shape[0], self.padded_input_tensor.shape[1], np.int(np.floor((self.conv_y_size - 1) / 2)),
+                                   self.padded_input_tensor.shape[3]])
+        y_right_padding = np.zeros([self.padded_input_tensor.shape[0], self.padded_input_tensor.shape[1], np.int(np.ceil((self.conv_y_size - 1) / 2)),
+                                    self.padded_input_tensor.shape[3]])
+
+        self.padded_input_tensor = np.concatenate([y_left_padding, self.padded_input_tensor], 2)
+        self.padded_input_tensor = np.concatenate([self.padded_input_tensor, y_right_padding], 2)
+
+
+
         for batch in np.arange(input_tensor.shape[0]):
 
             '''Calculate and Init the Output Tensor'''
@@ -53,27 +95,7 @@ class Conv:
 
             self.output_tensor = np.zeros([self.batch_num, self.num_kernels, self.out_y, self.out_x])
 
-            '''Same Padding: tries to pad evenly left and right, but if the amount of columns to be added is odd, 
-            it will add the extra column to the right, as is the case in this example (the same logic applies 
-            vertically: there may be an extra row of zeros at the bottom). In Same padding with stride length one 
-            the dimensions stay the same'''
 
-            '''Same - Padding is calculated as described above'''
-            x_left_padding = np.zeros([input_tensor[batch].shape[0], input_tensor[batch].shape[1],
-                                       np.int(np.floor((self.conv_x_size - 1) / 2))])
-            x_right_padding = np.zeros([input_tensor[batch].shape[0], input_tensor[batch].shape[1],
-                                        np.int(np.ceil((self.conv_x_size - 1) / 2))])
-
-            self.padded_input_tensor = np.concatenate([x_left_padding, input_tensor[batch]], 2)
-            self.padded_input_tensor = np.concatenate([self.padded_input_tensor, x_right_padding], 2)
-
-            y_left_padding = np.zeros([self.padded_input_tensor.shape[0], np.int(np.floor((self.conv_y_size - 1) / 2)),
-                                       self.padded_input_tensor.shape[2]])
-            y_right_padding = np.zeros([self.padded_input_tensor.shape[0], np.int(np.ceil((self.conv_y_size - 1) / 2)),
-                                        self.padded_input_tensor.shape[2]])
-
-            self.padded_input_tensor = np.concatenate([y_left_padding, self.padded_input_tensor], 1)
-            self.padded_input_tensor = np.concatenate([self.padded_input_tensor, y_right_padding], 1)
 
             '''Calculate Convolution for each kernel and each x and y dimension'''
             # loop over every y value with the right stride size
@@ -84,7 +106,7 @@ class Conv:
                     x_out = 0
                     for x in np.arange(0, self.input_x_size, self.x_stride):
                         # get the tensor which will be multiplied with the kernel
-                        tensor_for_multiply = self.padded_input_tensor[:, y: (y + self.conv_y_size),
+                        tensor_for_multiply = self.padded_input_tensor[batch][:, y: (y + self.conv_y_size),
                                               x:(x + self.conv_x_size)]
                         # make one convolution, first multiply with the weights and the sum over it to get one value
                         self.output_tensor[batch, kernel][y_out][x_out] = np.sum(
@@ -104,31 +126,36 @@ class Conv:
         '''Calculate Convolution for each kernel and each x and y dimension'''
         # loop over every y value with the right stride size
         self.gradient_weights = np.zeros([self.num_kernels, self.input_z_size, self.conv_y_size, self.conv_x_size])
+        self.error_tensor = np.zeros(self.padded_input_tensor.shape)
 
-        for batch in np.arange(self.batch_num):
-            for kernel in np.arange(self.num_kernels):
-                y_out = 0
-                for y in np.arange(0, self.input_y_size, self.y_stride):
-                    # loop over every x value with the right stride size
-                    x_out = 0
-                    for x in np.arange(0, self.input_x_size, self.x_stride):
-                        # get the tensor which will be multiplied with the kernel
-                        tensor_for_multiply = self.padded_input_tensor[:, y: (y + self.conv_y_size),
-                                              x:(x + self.conv_x_size)]
-                        # make one convolution, first multiply with the weights and the sum over it to get one value
-                        self.output_tensor[batch, kernel][y_out][x_out] = np.sum(
-                            np.multiply(tensor_for_multiply, self.weights[kernel])) + self.bias[kernel]
+        y_out = 0
+        for y in np.arange(0, self.input_y_size, self.y_stride):
+            # loop over every x value with the right stride size
+            x_out = 0
+            for x in np.arange(0, self.input_x_size, self.x_stride):
 
-                        '''Do The Backward Convolution'''
-                        tensor_for_multiply = self.padded_input_tensor[:, y: (y + self.conv_y_size),
-                                              x:(x + self.conv_x_size)]
+                '''Do The Backward Convolution'''
+                tensor_for_multiply = self.padded_input_tensor[:,:, y: (y + self.conv_y_size),
+                                      x:(x + self.conv_x_size)]
 
-                        backward_layers = backward_tensor.shape[1]
+                for kernel in np.arange(self.num_kernels):
+                    for batch in np.arange(self.batch_num):
+                        self.gradient_weights[kernel] += \
+                            tensor_for_multiply[batch]*backward_tensor[batch, kernel][y_out][x_out]
 
-                        for bl in np.arange(backward_layers):
-                            self.gradient_weights[bl] += tensor_for_multiply*backward_tensor[batch, bl][y_out][x_out]
+                        '''calc error tensor'''
+                        self.error_tensor[batch][:, y: (y + self.conv_y_size), x:(x + self.conv_x_size)] \
+                            += self.weights[kernel]*backward_tensor[batch, kernel][y_out][x_out]
 
+                x_out += 1
+            y_out += 1
 
 
-                        x_out += 1
-                    y_out += 1
+        y_end = self.error_tensor.shape[2]  - self.num_y_right_zeros
+        x_end = self.error_tensor.shape[3]  - self.num_x_right_zeros
+
+        '''Update Kernels'''
+        for kernel in np.arange(self.num_kernels):
+            self.weights[kernel] -= self.gradient_weights[kernel]
+
+        return self.error_tensor[:,:, self.num_y_left_zeros: y_end, self.num_x_left_zeros : x_end ]
--- Layers/Initializers.py
+++ Layers/Initializers.py
@@ -12,19 +12,22 @@ class Constant:
 
 class UniformRandom:
     def initialize(self, weights_shape, fan_in, fan_out):
-        self.weights = np.random.rand(fan_in , fan_out)
-        return self.weights
+        weights = np.random.rand(fan_in , fan_out)
+        weights = weights.reshape(weights_shape)
+        return weights
 
 
 class Xavier:
     def initialize(self, weights_shape, fan_in, fan_out):
-        self.sigma = np.sqrt(2/(fan_in + fan_out))
-        self.weights = np.random.normal(0, self.sigma, [fan_in, fan_out])
-        return self.weights
+        sigma = np.sqrt(2/(fan_in + fan_out))
+        weights = np.random.normal(0, sigma, [fan_in, fan_out])
+        weights = weights.reshape(weights_shape)
+        return weights
 
 
 class He:
     def initialize(self, weights_shape, fan_in, fan_out):
-        self.sigma = np.sqrt(2.0/fan_in)
-        self.weights = np.random.normal(0, self.sigma, [fan_in, fan_out])
-        return self.weights
+        sigma = np.sqrt(2.0/fan_in)
+        weights = np.random.normal(0, sigma, [fan_in, fan_out])
+        weights = weights.reshape(weights_shape)
+        return weights
