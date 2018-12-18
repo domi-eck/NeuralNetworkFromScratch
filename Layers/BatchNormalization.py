9900c223d39af7552cd887950850e7c6a6a09a02
schwarza <alexander.sch@gmx.de>
2018-12-18 15:12:20 +0100

    BatchNorm Forward passes

--- Layers/BatchNormalization.py
+++ Layers/BatchNormalization.py
@@ -6,6 +6,12 @@ class BatchNormalization(Base.Base):
         self.gamma = gamma
         self.phase = Base.Phase.train
     def forward(self, input_tensor):
+        IsConvInput = False
+        if input_tensor.ndim == 4:
+            input_tensor = np.transpose(input_tensor, (0, 3, 2, 1))
+            shapeBevorReshaping = input_tensor.shape
+            input_tensor = input_tensor.reshape(-1, input_tensor.shape[3])
+            IsConvInput = True
         if self.phase == Base.Phase.train:
             self.mean = np.mean(input_tensor, axis=0)
             self.var = np.var(input_tensor, axis=0)
@@ -14,10 +20,13 @@ class BatchNormalization(Base.Base):
                 nenner = np.sqrt(  self.var[a]   )
                 self.forward_output[:,a] = (input_tensor[:,a] - self.mean[a]) / nenner
         if self.phase == Base.Phase.test:
-            print("Mean: " + str( self.mean ) + " Var: " + str( self.var))
+            self.forward_output = np.zeros_like(input_tensor)
             for a in np.arange(input_tensor.shape[1]):
                 nenner = np.sqrt(  self.var[a] )
                 self.forward_output[:,a] = (input_tensor[:,a] - self.mean[a]) / nenner
+        if IsConvInput == True:
+            self.forward_output = self.forward_output.reshape(shapeBevorReshaping)
+            self.forward_output = np.transpose(self.forward_output, (0,3,2,1))
         return self.forward_output
     def backward(self, error_tensor):
         b = 2
