ce3e3fbac16aa423f3e5bb1b3fb2aefdab9f7448
Dominik Eckert <dominik.17.eckert@fau.de>
2018-12-18 15:19:41 +0100

    test forward batch normalization working

--- Layers/BatchNormalization.py
+++ Layers/BatchNormalization.py
@@ -7,23 +7,27 @@ class BatchNormalization(Base.Base):
         self.phase = Base.Phase.train
     def forward(self, input_tensor):
         if self.phase == Base.Phase.train:
-            self.mean = np.mean(input_tensor, axis=0)
-            self.var = np.var(input_tensor, axis=0)
-            self.forward_output = np.zeros_like(input_tensor)
-            for a in np.arange(input_tensor.shape[1]):
-                nenner = np.sqrt(  self.var[a]   )
-                self.forward_output[:,a] = (input_tensor[:,a] - self.mean[a]) / nenner
 
-            # self.forward_output = np.zeros_like(input_tensor)
-            # tensor = input_tensor
-            # tensor = np.transpose(tensor, (0, *range(2, tensor.ndim), 1))
-            # tensor = tensor.reshape(-1, 2)
-            # self.mean = np.mean(tensor, axis=0)
-            # self.var = np.var(tensor, axis=0)
-            # nenner = np.sqrt(self.var)
-            #
-            # for a in np.arange(input_tensor.shape[1]):
-            #     self.forward_output[:, a] = (input_tensor[:, a] - self.mean[a]) / nenner[a]
+            if input_tensor.ndim == 2:
+                self.mean = np.mean(input_tensor, axis=0)
+                self.var = np.var(input_tensor, axis=0)
+                self.forward_output = np.zeros_like(input_tensor)
+                for a in np.arange(input_tensor.shape[1]):
+                    nenner = np.sqrt(  self.var[a]   )
+                    self.forward_output[:,a] = (input_tensor[:,a] - self.mean[a]) / nenner
+
+            if input_tensor.ndim == 4:
+
+                self.forward_output = np.zeros_like(input_tensor)
+                tensor = input_tensor
+                tensor = np.transpose(tensor, (0, *range(2, tensor.ndim), 1))
+                tensor = tensor.reshape(-1, 2)
+                self.mean = np.mean(tensor, axis=0)
+                self.var = np.var(tensor, axis=0)
+                nenner = np.sqrt(self.var)
+
+                for a in np.arange(input_tensor.shape[1]):
+                    self.forward_output[:, a] = (input_tensor[:, a] - self.mean[a]) / nenner[a]
 
 
         if self.phase == Base.Phase.test:
@@ -31,6 +35,7 @@ class BatchNormalization(Base.Base):
             for a in np.arange(input_tensor.shape[1]):
                 nenner = np.sqrt(  self.var[a] )
                 self.forward_output[:,a] = (input_tensor[:,a] - self.mean[a]) / nenner
+
         return self.forward_output
     def backward(self, error_tensor):
         b = 2
