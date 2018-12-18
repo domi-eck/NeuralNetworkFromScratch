b68b47a75b68c990a410740e350cfa4334daded7
Dominik Eckert <dominik.17.eckert@fau.de>
2018-12-18 15:04:04 +0100

    batch norm convolution shape forward working but not with normal array

--- Layers/BatchNormalization.py
+++ Layers/BatchNormalization.py
@@ -13,6 +13,19 @@ class BatchNormalization(Base.Base):
             for a in np.arange(input_tensor.shape[1]):
                 nenner = np.sqrt(  self.var[a]   )
                 self.forward_output[:,a] = (input_tensor[:,a] - self.mean[a]) / nenner
+
+            # self.forward_output = np.zeros_like(input_tensor)
+            # tensor = input_tensor
+            # tensor = np.transpose(tensor, (0, *range(2, tensor.ndim), 1))
+            # tensor = tensor.reshape(-1, 2)
+            # self.mean = np.mean(tensor, axis=0)
+            # self.var = np.var(tensor, axis=0)
+            # nenner = np.sqrt(self.var)
+            #
+            # for a in np.arange(input_tensor.shape[1]):
+            #     self.forward_output[:, a] = (input_tensor[:, a] - self.mean[a]) / nenner[a]
+
+
         if self.phase == Base.Phase.test:
             print("Mean: " + str( self.mean ) + " Var: " + str( self.var))
             for a in np.arange(input_tensor.shape[1]):
