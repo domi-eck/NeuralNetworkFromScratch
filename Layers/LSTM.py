29331d1d47aaa19fc3b8bb9bb66652f612ae67c3
schwarza <alexander.sch@gmx.de>
2019-01-28 23:30:13 +0100

    adding backward methods es fehlt noch die get und set gradient test_gradient müsste auch óhne die methoden funktionieren es gibt wahrscheinlich eine unterschied zwischen forward und backward

--- Layers/LSTM.py
+++ Layers/LSTM.py
@@ -2,6 +2,7 @@ import numpy as np
 from Layers import Sigmoid
 from Layers import TanH
 from Layers import FullyConnected
+from Layers import Initializers
 import copy
 from Optimization import Optimizers
 
@@ -14,8 +15,9 @@ class LSTM:
         self.bptt_length = bptt_lenght
         #endregion Sizes
 
+        self.learning_rate = 0.001
         self.hidden_state = np.zeros([self.bptt_length, self.hidden_size])
-        self.cell_state = np.zeros([self.bptt_length, 4 * self.hidden_size])
+        self.cell_state   = np.zeros([self.bptt_length, 4 * self.hidden_size])
 
         #region Sigmoids
         self.sigmoid_f = []
@@ -71,6 +73,8 @@ class LSTM:
         for i in np.arange(0, self.bptt_length):
             a = FullyConnected.FullyConnected(4*self.hidden_size, self.hidden_size)
             self.list_fully_connected_ch.append(a)
+
+        self.initialize(Initializers.He(), Initializers.He())
         #endregion FullyConnected
 
         #region Metavariables
@@ -82,6 +86,13 @@ class LSTM:
         self.optimizer = None
         #endregion Optimizer
 
+        #region Input for Multiplication
+        self.f              = np.zeros([self.bptt_length, 4*self.hidden_size])
+        self.i              = np.zeros([self.bptt_length, 4*self.hidden_size])
+        self.c_tilde        = np.zeros([self.bptt_length, 4*self.hidden_size])
+        self.tanhO          = np.zeros([self.bptt_length, self.hidden_size])
+        self.o              = np.zeros([self.bptt_length, self.hidden_size])
+        #endregion
 
     def toggle_memory(self):
         if self.same_sequence:
@@ -90,9 +101,8 @@ class LSTM:
             self.same_sequence = True
 
     def forward(self, input_tensor):
-        #concatination
         self.input_tensor = input_tensor
-        self.output         = np.zeros([self.bptt_length, self.output_size])
+        self.output       = np.zeros([self.bptt_length, self.output_size])
 
         if self.same_sequence is False:
             self.hidden_state = np.zeros([self.bptt_length , self.hidden_size])
@@ -103,26 +113,26 @@ class LSTM:
 
             #region Calculating f, i, ctilde, o
             #f
-            f       = self.list_fully_connected_f[time].forward(np.expand_dims(self.x_tilde, 0))[0]
-            f       = self.sigmoid_f[time].forward(f)
-            i       = self.list_fully_connected_i[time].forward(np.expand_dims(self.x_tilde, 0))[0]
-            i       = self.sigmoid_i[time].forward(i)
-            c_tilde = self.list_fully_connected_c[time].forward(np.expand_dims(self.x_tilde, 0))[0]
-            c_tilde = self.tanh_c[time].forward(c_tilde)
-            o       = self.list_fully_connected_o[time].forward(np.expand_dims(self.x_tilde, 0))[0]
-            o       = self.sigmoid_o[time].forward(o)
+            f                   = self.list_fully_connected_f[time].forward(np.expand_dims(self.x_tilde, 0))[0]
+            self.f[time]        = self.sigmoid_f[time].forward(f)
+            i                   = self.list_fully_connected_i[time].forward(np.expand_dims(self.x_tilde, 0))[0]
+            self.i[time]        = self.sigmoid_i[time].forward(i)
+            c_tilde             = self.list_fully_connected_c[time].forward(np.expand_dims(self.x_tilde, 0))[0]
+            self.c_tilde[time]  = self.tanh_c[time].forward(c_tilde)
+            o                   = self.list_fully_connected_o[time].forward(np.expand_dims(self.x_tilde, 0))[0]
+            self.o[time]        = self.sigmoid_o[time].forward(o)
             #endregion
 
             #region Calculating C
-            iAndc_tilde                                     = np.multiply(i, c_tilde)
-            fAndc                                           = np.multiply(self.cell_state[time], f)
+            iAndc_tilde                         = np.multiply(self.i[time], self.c_tilde[time])
+            fAndc                              = np.multiply(self.cell_state[time], self.f[time])
             self.cell_state[(time + 1) % self.bptt_length]  = fAndc + iAndc_tilde
             #endregion
 
             #region Updating HiddenState
-            tanhO                                               = self.list_fully_connected_ch[time].forward(np.expand_dims(self.cell_state[(time + 1) % self.bptt_length], 0))[0]
-            tanhO                                               = self.tanh_o[time].forward(tanhO)
-            self.hidden_state[(time + 1) % self.bptt_length]    = np.multiply(tanhO, o)
+            self.tanhO[time]                                    = self.list_fully_connected_ch[time].forward(np.expand_dims(self.cell_state[(time + 1) % self.bptt_length], 0))[0]
+            self.tanhO[time]                                    = self.tanh_o[time].forward(self.tanhO[time])
+            self.hidden_state[(time + 1) % self.bptt_length]    = np.multiply(self.tanhO[time], o)
             #endregion
 
             #region Calculating Output
@@ -133,9 +143,95 @@ class LSTM:
 
         return self.output
 
-
     def backward(self, error_tensor):
-        dummy = 1
+        self.sum_Gradients_f = np.zeros_like(self.list_fully_connected_f[-1].get_weights())
+        self.sum_Gradients_i = np.zeros_like(self.list_fully_connected_i[-1].get_weights())
+        self.sum_Gradients_c = np.zeros_like(self.list_fully_connected_c[-1].get_weights())
+        self.sum_Gradients_o = np.zeros_like(self.list_fully_connected_o[-1].get_weights())
+        self.sum_Gradients_y = np.zeros_like(self.list_fully_connected_y[-1].get_weights())
+        self.sum_Gradients_ch = np.zeros_like(self.list_fully_connected_ch[-1].get_weights())
+
+        self.error_tensor   = error_tensor
+        self.error_ht1      = np.zeros([self.bptt_length, self.hidden_size])
+        self.error_cell     = np.zeros([self.bptt_length, 4*self.hidden_size])
+        self.output_x       = np.zeros([self.bptt_length, self.input_size])
+
+        if self.same_sequence is False:
+            self.error_ht1 = np.zeros([self.bptt_length ,self.hidden_size])
+            self.error_cell = np.zeros([self.bptt_length, 4*self.hidden_size])
+
+        for time in np.arange(error_tensor.shape[0])[::-1]:
+            error = self.error_tensor[time]
+
+            dh = self.list_fully_connected_y[time].backward(np.expand_dims(error,0))
+            if time != self.bptt_length-1:
+                dh = dh + self.error_ht1[time+1]
+
+            bo = np.multiply( self.tanhO[time], dh )
+            bo = self.sigmoid_o[time].backward(bo)
+            bo = self.list_fully_connected_o[time].backward(bo)
+
+            dc = np.multiply( self.o[time], dh )
+            dc = self.tanh_o[time].backward(dc)
+            dc = self.list_fully_connected_ch[time].backward(dc)
+            if time != self.bptt_length-1:
+                dc = dc + self.error_cell[time+1]
+
+            self.error_cell[time] = np.multiply( self.f[time], dc )
+            bf = self.cell_state[time] * dc
+            bf = self.sigmoid_f[time].backward(bf)
+            bf = self.list_fully_connected_f[time].backward(bf)
+
+            bi          = np.multiply( self.c_tilde[time], dc )
+            bi          = self.sigmoid_i[time].backward(bi)
+            bi          = self.list_fully_connected_i[time].backward(bi)
+
+            bc_tilde    = np.multiply(self.i[time], dc)
+            bc_tilde    = self.tanh_c[time].backward(bc_tilde)
+            bc_tilde    = self.list_fully_connected_c[time].backward(bc_tilde)
+
+            xh = bc_tilde + bi + bf + bo
+
+            #Get gradients
+            self.sum_Gradients_f = self.sum_Gradients_f + self.list_fully_connected_f[time].get_gradient_weights()
+            self.sum_Gradients_i = self.sum_Gradients_i + self.list_fully_connected_i[time].get_gradient_weights()
+            self.sum_Gradients_c = self.sum_Gradients_c + self.list_fully_connected_c[time].get_gradient_weights()
+            self.sum_Gradients_o = self.sum_Gradients_o + self.list_fully_connected_o[time].get_gradient_weights()
+            self.sum_Gradients_y = self.sum_Gradients_y + self.list_fully_connected_y[time].get_gradient_weights()
+            self.sum_Gradients_ch = self.sum_Gradients_ch + self.list_fully_connected_ch[time].get_gradient_weights()
+
+
+           # self.error_ht[time] = xh[0][self.input_size:]
+            self.output_x[time] = xh[0][:self.input_size]
+
+        #region Update
+        # optimize the weights
+        if self.has_optimizer is True:
+            self.f_weights = self.optimizer.calculate_update(self.learning_rate, self.f_weights, self.sum_Gradients_f)
+            self.i_weights = self.optimizer.calculate_update(self.learning_rate, self.i_weights, self.sum_Gradients_i)
+            self.c_weights = self.optimizer.calculate_update(self.learning_rate, self.c_weights, self.sum_Gradients_c)
+            self.o_weights = self.optimizer.calculate_update(self.learning_rate, self.o_weights, self.sum_Gradients_o)
+            self.y_weights = self.optimizer.calculate_update(self.learning_rate, self.y_weights, self.sum_Gradients_y)
+            self.ch_weights = self.optimizer.calculate_update(self.learning_rate, self.ch_weights, self.sum_Gradients_ch)
+
+            for layer in self.list_fully_connected_f:
+                layer.set_weights(self.f_weights)
+            for layer in self.list_fully_connected_i:
+                layer.set_weights(self.i_weights)
+            for layer in self.list_fully_connected_c:
+                layer.set_weights(self.c_weights)
+            for layer in self.list_fully_connected_o:
+                layer.set_weights(self.o_weights)
+            for layer in self.list_fully_connected_y:
+                layer.set_weights(self.y_weights)
+            for layer in self.list_fully_connected_ch:
+                layer.set_weights(self.ch_weights)
+
+        #endregion
+
+        return  self.output_x
+
+
 
     def get_gradient_weights(self):
         dummy = 1
